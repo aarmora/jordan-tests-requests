078c4c253b433a035f4c86d73d2f244926d11dac
Jordan Hansen <jbhansen84@gmail.com>
2020-05-23 10:42:11 -0600

    add this stuff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/../.env' });
 
 (async () => {
 
-	const url = 'https://jordan-tests-requests-express-server-nbxxguimx.now.sh/api';
+	// const url = `http://api.scraperapi.com?api_key=${process.env.scraperApiKey}&url=https://jordan-tests-requests-express-server-nbxxguimx.now.sh/api`;
+	const url = `https://jordan-tests-requests-express-server-nbxxguimx.now.sh/api`;
 
 	const axiosResponse = await axios.post(url);
 
